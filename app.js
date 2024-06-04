9519e637b1c0a31f35cf29e1c7ef899c10a5daf7
asharma45822 <159045776+asharma45822@users.noreply.github.com>
2024-06-04 18:06:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-<script>
+
             $(document).ready(function () {
                 $('#enquiryForm').on('submit', function (e) {
                     e.preventDefault();
                     alert('Enquiry Sent! Thank you for contacting us.');
                 });
             });
-        </script>
+      
